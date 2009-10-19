b6af8a5d2fc4123ae99381fc46663718f1802097
odwyerrob <odwyerrob@578f2612-a7b3-11de-8dbb-b5444cbab91d>
2009-10-19 20:13:35 +0000

    Motor control is working, plus some path/checkpoint stuff

--- trunk/rover/slave.h
+++ trunk/rover/slave.h
@@ -9,8 +9,16 @@
 /* DC motors */
 #define MOTORL1 OCR0A
 #define MOTORL2 OCR0B
-#define MOTORR1 OCR2A
-#define MOTORR2 OCR2B
+#define MOTORR1 OCR1A
+#define MOTORR2 OCR1B
+
+#define MOTORL_PORT PORTD
+#define MOTORR_PORT PORTB
+
+#define MOTORL1_PIN 6
+#define MOTORL2_PIN 5
+#define MOTORR1_PIN 1
+#define MOTORR2_PIN 2
 
 #define MOTORL_FORWARD(x) {MOTORL1 = x; MOTORL2 = 0;}
 #define MOTORL_REVERSE(x) {MOTORL1 = 0; MOTORL2 = x;}
@@ -21,10 +29,9 @@
 #define MOTORR_BRAKE(x) {MOTORR1 = x; MOTORR2 = x;}
 
 /* Servo motor(s) */
-#define SERVO_MIN 950
-#define SERVO1 OCR1A
-#define SERVO2 OCR1B
-#define SERVO_POSITION(x) (x + SERVO_MIN) // Input should be from 0-3000
+#define SERVO_MIN 950 // TODO: recalculate this value for 8-bit control
+#define SERVO1 OCR2B
+#define SERVO_POSITION(x) (x + SERVO_MIN)
 
 /* TWI Definitions */
 #define TWI_SLAVE 0x5A
@@ -45,11 +52,39 @@
 #define REVERSE_RIGHT 6
 #define GET_PATH_OFFSET 7
 
+/* Data types */
+struct checkpoint {
+	uint16_t turn_angle; /* Direction of the checkpoint from last in degrees */
+	uint16_t distance; /* Distance to this checkpoint (meters? centimetres?) */
+	uint8_t radius; /* Distance from the checkpoint centre that's safe to turn in */
+	uint8_t sensor_flags; /* Bitfield indicating sensors that can be trusted near checkpoint */
+}
+
+
+/* Global variables */
+// TWI buffer
+volatile uint8_t rxBuffer[24];
+volatile uint8_t i = 0;
+
+// Checkpoints
+checkpoint **path;
+checkpoint *goal;
+
 /* Function prototypes */
 void init(void);
 void show_error(uint8_t code);
 void twi_rx(uint8_t* buffer, int count);
 void twi_tx(void);
+
+
+
+
+
+
+
+
+
+
 
 
 #endif /* end of include guard: _testing_ */
