5f960737e622b0bf528a96463ef8e7bc7bc6aa84
odwyerrob <odwyerrob@578f2612-a7b3-11de-8dbb-b5444cbab91d>
2009-10-28 02:35:23 +0000

    Basic path following is now working

--- rover/slave.h
+++ rover/slave.h
@@ -1,10 +1,22 @@
 #ifndef _testing_
 #define _testing_
 
+//#define SQUARE_TRACK
+#define ZIGZAG_TRACK
+
 /* Clock speed of the AVR CPU */
 #ifndef F_CPU
 #define F_CPU 16000000
 #endif
+
+#define STARTUP_DELAY 2000
+
+#define SERIAL_ENABLED 1
+
+/* Status LED */
+#define LED_PORT PORTB
+#define LED_DDR DDRB
+#define LED_PIN 0
 
 /* DC motors */
 #define MOTORL1 OCR0A
@@ -13,7 +25,9 @@
 #define MOTORR2 OCR1B
 
 #define MOTORL_PORT PORTD
+#define MOTORL_DDR DDRD
 #define MOTORR_PORT PORTB
+#define MOTORR_DDR DDRB
 
 #define MOTORL1_PIN 6
 #define MOTORL2_PIN 5
@@ -28,12 +42,18 @@
 #define MOTORR_REVERSE(x) {MOTORR1 = 0; MOTORR2 = x;}
 #define MOTORR_BRAKE(x) {MOTORR1 = x; MOTORR2 = x;}
 
+#define MS_PER_DEGREE 11
+#define MS_PER_METRE 20
+#define BRAKE_TIME 1000
+
 /* Servo motor(s) */
 #define SERVO_MIN 950 // TODO: recalculate this value for 8-bit control
 #define SERVO1 OCR2B
 #define SERVO_POSITION(x) (x + SERVO_MIN)
 
 /* TWI Definitions */
+#define TWI_ENABLED 0
+
 #define TWI_SLAVE 0x5A
 #define TWI_BAD_LENGTH 1
 #define TWI_ADDR_NACK 2
@@ -54,30 +74,41 @@
 
 /* Data types */
 struct checkpoint {
-	uint16_t turn_angle; /* Direction of the checkpoint from last in degrees */
 	uint16_t distance; /* Distance to this checkpoint (meters? centimetres?) */
+	int16_t angle; /* Direction of the checkpoint from last in degrees */
 	uint8_t radius; /* Distance from the checkpoint centre that's safe to turn in */
 	uint8_t sensor_flags; /* Bitfield indicating sensors that can be trusted near checkpoint */
-}
-
+} __attribute__((__packed__));
 
 /* Global variables */
 // TWI buffer
 volatile uint8_t rxBuffer[24];
 volatile uint8_t i = 0;
 
-// Checkpoints
-checkpoint **path;
-checkpoint *goal;
+// Current checkpoint
+struct checkpoint *goal;
 
 /* Function prototypes */
 void init(void);
-void show_error(uint8_t code);
+
+void turnRightTo(int16_t degree);
+void turnLeftTo(int16_t degree);
+void driveUntil(uint16_t distance, uint8_t speed);
+void brake(void);
+
 void twi_rx(uint8_t* buffer, int count);
 void twi_tx(void);
 
+void LED_ON(void);
+void LED_OFF(void);
 
-
+void DEBUG_STRING(const char *str);
+void DEBUG_NUMBER(const char *name, uint16_t num);
+#if(SERIAL_ENABLED)
+#define DEBUG_CHAR(x) uart_putc(x)
+#else
+#define DEBUG_CHAR(x)
+#endif
 
 
 
---
+++ rover/squaretrack.h
@@ -0,0 +1,44 @@
+#ifndef SQUARETRACK_H
+#define SQUARETRACK_H
+
+/* TODO: store this in FLASH memory, not SRAM */
+
+struct checkpoint track[] = {
+	/* First straight stretch */
+	{
+		.distance = 100,
+		.angle = 0,
+		.radius = 20,
+		.sensor_flags = 0x00
+	},
+	/* Turn 90deg and go straight for 100m */
+	{
+		.distance = 100,
+		.angle = 90,
+		.radius = 20,
+		.sensor_flags = 0x00
+	},
+	/* Turn 90deg and go straight for 100m */
+	{
+		.distance = 100,
+		.angle = 90,
+		.radius = 20,
+		.sensor_flags = 0x00
+	},
+	/* Turn 90deg and go straight for 100m */
+	{
+		.distance = 100,
+		.angle = 90,
+		.radius = 20,
+		.sensor_flags = 0x00
+	},
+	/* End of track */
+	{
+		.distance = 0,
+		.angle = 0,
+		.radius = 0,
+		.sensor_flags = 0
+	}
+};
+
+#endif /* end of include guard: SQUARETRACK_H */
